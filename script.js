b72e95a68f69589599ce80930b532686b675b170
orzcode <71000311+orzcode@users.noreply.github.com>
2023-02-11 10:39:52 +1300

    favicon and array with sample entries

---
+++ script.js
@@ -0,0 +1,26 @@
+let library = [];
+///////////////////
+let sampleBook1 = {
+	"title": "Dune",
+	"author": "Frank Herbert",
+	"pages": 300,
+	"genre": "Sci-fi",
+	"comments": "Exciting and fun"
+   }
+library.push(sampleBook1);
+
+let sampleBook2 = {
+	"title": "Dune Messiah",
+	"author": "Frank Herbert",
+	"pages": 250,
+	"genre": "Sci-fi",
+	"comments": "More political than the first"
+   }
+library.push(sampleBook2);
+////////////////////
+
+
+
+function Book(title, author, pages, genre, comments) {
+
+}
