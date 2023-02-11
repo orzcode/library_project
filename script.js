a40bf63390dda42e9818d7d2c09940a8ec3e2d30
orzcode <71000311+orzcode@users.noreply.github.com>
2023-02-11 15:41:31 +1300

    book array function scaffolding

--- script.js
+++ script.js
@@ -20,7 +20,23 @@ library.push(sampleBook2);
 ////////////////////
 
 
-
 function Book(title, author, pages, genre, comments) {
-
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.genre = genre;
+	this.comments = comments;
 }
+
+///////////////
+library.push(new Book("The Bible", "God", "1000", "Fantasy", "Boring"));
+
+
+console.log(library);
+///////////////
+function propagateBooks(){
+	for(let i = 0; i < library.length; i++){
+		console.log(library[i].title);
+	}
+}
+propagateBooks();
