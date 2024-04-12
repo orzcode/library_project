4a87cdaf921566ec8ac561205bc129c1e4a199b7
orzcode <71000311+orzcode@users.noreply.github.com>
2024-04-12 16:55:03 +1200

    modularize firebasestuff
    CSS seems to be wonky

--- index.js
+++ index.js
@@ -1,77 +1,8 @@
 let library = [];
 let libraryDiv = document.querySelector("#library");
-////////////////////////////////////////////////////
-
-///////////////////
-//FIREBASE
-///////////////////
-import firebase from "firebase/compat/app";
-import * as firebaseui from "firebaseui";
-import "firebaseui/dist/firebaseui.css";
-
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCgWOsD40-y422erIMNultdmSBmcP5c_VY",
-  authDomain: "tv-series-library.firebaseapp.com",
-  databaseURL: "https://tv-series-library-default-rtdb.firebaseio.com",
-  projectId: "tv-series-library",
-  storageBucket: "tv-series-library.appspot.com",
-  messagingSenderId: "371898195484",
-  appId: "1:371898195484:web:6d181e6ccf75b8410ec9d9",
-};
-
-// Initialize Firebase app
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
-const db = getFirestore(app);
-
-const uiConfig = {
-  signInOptions: [
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-      requireDisplayName: false,
-      forceSameDevice: false,
-    },
-  ],
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function () {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById("loader").style.display = "none";
-    },
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: "popup",
-  signInSuccessUrl: "<url-to-redirect-to-on-success>",
-};
-
-// Check for pending redirect
-if (firebaseui.auth.AuthUI.getInstance() && firebaseui.auth.AuthUI.getInstance().isPendingRedirect()) {
-  firebaseui.auth.AuthUI.getInstance().start('#firebaseui-auth-container', uiConfig);
-} else {
-  // Initialize the FirebaseUI Widget using Firebase.
-  const ui = new firebaseui.auth.AuthUI(auth);
-  // The start method will wait until the DOM is loaded.
-  ui.start("#firebaseui-auth-container", uiConfig);
-}
-//FIREBASE
-///////////////////
-//https://firebase.google.com/docs/auth/web/firebaseui?authuser=0&hl=en
-////////////////////
-
-
+/////////////////////////////////////////////////////////
+import { auth, database, db, uiConfig } from "./firebasestuff";
+/////////////////////////////////////////////////////////
 
 
 //Series constructor function//
