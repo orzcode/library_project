b5768da785b4b032b65c5a182fe74819fddfeed5
orzcode <71000311+orzcode@users.noreply.github.com>
2024-04-16 14:57:41 +1200

    prep for layout update
    and updated fontawesome css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let library = [];
 let libraryDiv = document.querySelector("#library");
 /////////////////////////////////////////////////////////
-import { auth, uiConfig } from "./firebasestuff";
+//import { auth, uiConfig } from "./firebasestuff";
 /////////////////////////////////////////////////////////
 
 //Series constructor function//
