b1bde9646ddc3732b3aabda36847d5028d082a46
orzcode <71000311+orzcode@users.noreply.github.com>
2024-05-02 22:49:59 +1200

    modulize setcolors()

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import setColors from "./colors.js";
+setColors()
 ////////////////////////////////////////////////////////
 export { authCheck };
 ////////////////////////////////////////////////////////
@@ -502,53 +504,4 @@ async function getDataFromTVMaze(searchTerm) {
 }
 //--------------------------------------------------------//
 
-//--------------------------------------------------------//
-// Define an array of color schemes
-const colorSchemes = [
-  {
-    header: "#22334B",
-    main: "#2e2b2d",
-    highlighted: "34, 51, 75",
-  },
-  {
-    header: "#36221f",
-    main: "#6e5c5c",
-    highlighted: "54, 34, 31",
-  },
-  {
-    header: "#771c11",
-    main: "#342321",
-    highlighted: "119, 28, 17",
-  },
-];
 
-let currentSchemeIndex = parseInt(localStorage.getItem("currentSchemeIndex"));
-
-if (isNaN(currentSchemeIndex)) {
-  currentSchemeIndex = 0;
-}
-
-// Set the initial color scheme on page load
-function setColors() {
-  if (currentSchemeIndex !== undefined) {
-    let currentScheme = colorSchemes[currentSchemeIndex];
-    document.documentElement.style.setProperty(
-      "--header",
-      currentScheme.header
-    );
-    document.documentElement.style.setProperty("--main", currentScheme.main);
-    document.documentElement.style.setProperty(
-      "--highlighted",
-      currentScheme.highlighted
-    );
-  }
-}
-setColors();
-
-// Update the color scheme on button click
-document.querySelector("#colors").addEventListener("click", function () {
-  currentSchemeIndex = (currentSchemeIndex + 1) % colorSchemes.length;
-  setColors();
-  localStorage.setItem("currentSchemeIndex", currentSchemeIndex);
-});
-//--------------------------------------------------------//
