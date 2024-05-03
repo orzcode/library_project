d32d4b36dabcf4b6938d1e44cc0e7bf6fc6a4087
orzcode <71000311+orzcode@users.noreply.github.com>
2024-05-04 10:39:23 +1200

    modularize import/export

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import setColors from "./colors.js";
 setColors();
-////////////////////////////////////////////////////////
-export { authCheck };
+import { sendLibrary, getLibrary } from "./importExport.js";
 ////////////////////////////////////////////////////////
 import { auth, uiConfig, ui, UserLibrary } from "./firebasestuff.js";
 /////////////////////////////////////////////////////////
 let library = [];
 const libraryDiv = document.querySelector("#library");
 /////////////////////////////////////////////////////////
+export {authCheck, library}
+////////////////////////////////////////////////////////
 // Loader needed in all 3 cases:
 // Pageload into a signed-out state
 // Pageload into a signed-in state
@@ -179,16 +180,7 @@ function toggle(theObj) {
 }
 window.Series = Series;
 ////////////////////////////////////////////////////////////
-//Reconstructs a Series array from raw string data - to use, call this onto a new var
-function reSeries(unconstructedArray) {
-  if (unconstructedArray) {
-    return unconstructedArray.map(
-      ({ title, complete, link, image }) =>
-        new Series(title, complete, link, image)
-    );
-  } else return;
-}
-///////////////////
+
 // Runs when hitting 'Save' from the dialog screen
 export function submissionTasks() {
   const obj = formSubmission();
@@ -244,46 +236,6 @@ export function closeModal() {
 window.closeModal = closeModal;
 /////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////
-//-----------------------------------------------------------------------------//
-async function sendLibrary() {
-  const data = JSON.stringify(library);
-
-  if (data != []) {
-    localStorage.setItem("localContent", data);
-    //set a localStorage item called "localContent", set it as a stringified library[]
-    //I.E: takes whatever is in the session-based Library[] and saves/sends
-    UserLibrary.updateUserLibrary(data);
-    //also updates firebase Library
-  } else return;
-}
-
-async function getLibrary() {
-  let firebaseData = await UserLibrary.getUserLibrary();
-
-  let localData = localStorage.getItem("localContent");
-
-  // Parse and convert data to array of objects
-  const rematerialize = (data) => {
-    data = JSON.parse(data);
-    data = reSeries(data);
-    // Add to library
-    data.forEach((ObjFromArray) => {
-      library.push(ObjFromArray);
-    });
-  };
-
-  if (firebaseData !== null && firebaseData !== undefined && !(firebaseData instanceof Error)) {
-    // If Firebase Library has data, use it
-    rematerialize(firebaseData);
-  } else if (localData !== null && localData != undefined) {
-    // If Firebase Library doesn't have data but localStorage does, use localStorage data
-    rematerialize(localData);
-  } else {
-    // If neither Firebase Library nor localStorage have data, return
-    return;
-  }
-}
-//-----------------------------------------------------------------------------//
 
 //This function runs when you click 'Query'
 /////////////////////
